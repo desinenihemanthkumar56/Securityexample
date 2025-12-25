bcf515d72500435e4ae952f61c0915a50ad2d855
desinenihemanthkumar56 <desinenihemanthkumar@gmail.com>
2025-12-25 10:54:28 +0530

    24/12/25 class

---
+++ src/main/java/com/Securityexample/config/JWTFilter.java
@@ -0,0 +1,48 @@
+package com.Securityexample.config;
+
+import com.Securityexample.Repository.UserRepository;
+import com.Securityexample.Service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JWTFilter  extends OncePerRequestFilter{
+
+ @Autowired
+ private UserRepository userRepository;
+
+ @Autowired
+ private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader= request.getHeader("Authorization"); // to get the token
+        if(authHeader!=null && authHeader.startsWith("Bearer ")){
+            String token= authHeader.substring(7);
+            String username =jwtService.validateTokenAndRetrieveSubject(token);
+           if(username  !=null && SecurityContextHolder.getContext().getAuthentication()==null){
+
+               var userDetails = userRepository.findByUsername(username);
+               var authToken= new UsernamePasswordAuthenticationToken(userDetails,null, Collections.singleton(new SimpleGrantedAuthority(userDetails.getRole())));
+
+               authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+               SecurityContextHolder.getContext().setAuthentication(authToken);
+           }
+
+        }
+         filterChain.doFilter(request,response);
+        }
+    }
+
--- src/main/java/com/Securityexample/config/SecurityConfig.java
+++ src/main/java/com/Securityexample/config/SecurityConfig.java
@@ -57,6 +57,9 @@ public class SecurityConfig {
         return  Provider;
     }
 
+
+
+
    }
 
 
